bfc30cc94bb8b8daeee516b70379fe3572a1bf5e
huuanh2512 <huuanhnguyen56@gmail.com>
2025-07-08 10:51:28 +0700

    Chuyển layout FormRowAd từ grid sang flex để cải thiện khả năng responsive

--- client/src/components/FormRowAd.js
+++ client/src/components/FormRowAd.js
@@ -1,8 +1,10 @@
 const FormRowAd = ({ label, children }) => {
     return (
-        <div className="grid grid-cols-12 items-center mb-4 w-[88%] mx-auto">
-            <label className="col-span-3 text-base text-gray-600">{label}</label>
-            <div className="col-span-9 relative">{children}</div>
+        <div className="flex flex-col sm:flex-row sm:items-start my-4 w-[88%] mx-auto border">
+            <label className="sm:w-1/4 text-gray-600 pt-2">
+                {label}
+            </label>
+            <div className="flex-1">{children}</div>
         </div>
     );
 };
