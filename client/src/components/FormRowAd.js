db41eb228a05949d8a9605af2011f1cfe8891c2b
huuanh2512 <huuanhnguyen56@gmail.com>
2025-07-09 20:11:46 +0700

    Bỏ border cho việc kiểm tra giao diện lúc trước

--- client/src/components/FormRowAd.js
+++ client/src/components/FormRowAd.js
@@ -1,6 +1,6 @@
 const FormRowAd = ({ label, children }) => {
     return (
-        <div className="flex flex-col sm:flex-row sm:items-start my-4 w-[88%] mx-auto border">
+        <div className="flex flex-col sm:flex-row sm:items-start my-4 w-[88%] mx-auto">
             <label className="sm:w-1/4 text-gray-600 pt-2">
                 {label}
             </label>
